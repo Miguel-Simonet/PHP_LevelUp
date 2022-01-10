76353eddd9ff36ee36ae636ea0f4b755be51a6aa
Miguel-Simonet <84403278+Miguel-Simonet@users.noreply.github.com>
2022-01-10 21:02:04 +0100

    Form Correcto----10/01/2022

--- Front-End/Componentes/Carrusel/Back-end-Carrusel/db.php
+++ Front-End/Componentes/Carrusel/Back-end-Carrusel/db.php
@@ -4,6 +4,17 @@ function mysql_levelUp(){
 }
 function insert_img($titulo,$descripcion,$image){
     $mysql= mysql_levelUp();
-    $mysql->query("insert into carrusel (titulo,descripcion,image,created_at) value (\"$titulo\",\"$descripcion\",\"$image\",NOW())")
+    $mysql->query("insert into carrusel (titulo,descripcion,image,created_at) value (\"$titulo\",\"$descripcion\",\"$image\",NOW())");
+}
+$titulo_2=trim(strip_tags($_POST["titulo"]));
+$descripcion_2=trim(strip_tags($_POST["descripcion"])); 
+$image_2=$_POST["imagen"];
+if ($titulo_2!="" && $descripcion_2!="" && $image_2!=""){
+    insert_img($titulo_2,$descripcion_2,$image_2);
+    setcookie("correccion", "Datos validados", time()+10);
+    header('Location: index.php');
+}else{
+    setcookie("correccion", "Algun campo vacio", time()+10);
+    header('Location: index.php');
 }
 ?>
---
+++ Front-End/Componentes/Carrusel/Back-end-Carrusel/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--Boostrap-->
+    <?php
+            include("../../../../Boostrap_Thinks/boostrap.php");
+    ?>
+</head>
+<body>
+    <form action="db.php" method="post">
+        <div class="position-relative">
+        <div class="card mx-auto mt-4 " style="max-width: 470px; align-items:center;">
+            <div class="card-body">
+                <h5 class="card-title">Formulario carrusel</h5>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Titulo</label>
+                    <input type="name" class="form-control" id="titulo" name="titulo">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <div class="form-floating">
+                        <textarea class="form-control"  id="descripcion" name="descripcion" style="height: 100px"></textarea>
+                        <label for="floatingTextarea2">Descripción</label>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="formFileMultiple" class="form-label">Imagen</label>
+                    <input class="form-control" type="file" id="imagen" name="imagen" multiple>
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-secondary" type="submit">Button</button>
+                    <div id="emailHelp" class="form-text mx-auto">
+                        <?=isset($_COOKIE["correccion"]) ? $_COOKIE["correccion"] : "";?>
+
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </form>
+</body>
+</html>

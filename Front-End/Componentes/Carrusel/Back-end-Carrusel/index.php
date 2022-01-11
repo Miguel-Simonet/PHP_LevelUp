71690eaeab28613d5d8d5aada3c8d535da772dea
Miguel-Simonet <84403278+Miguel-Simonet@users.noreply.github.com>
2022-01-11 16:36:46 +0100

    11/01/2022

--- Front-End/Componentes/Carrusel/Back-end-Carrusel/index.php
+++ Front-End/Componentes/Carrusel/Back-end-Carrusel/index.php
@@ -19,7 +19,7 @@
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Titulo</label>
                     <input type="name" class="form-control" id="titulo" name="titulo">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <div id="emailHelp" class="form-text">Titulo solo con letras y menor a 15 caracteres.</div>
                 </div>
                 <div class="mb-3">
                     <div class="form-floating">
@@ -30,7 +30,7 @@
                 <div class="mb-3">
                     <label for="formFileMultiple" class="form-label">Imagen</label>
                     <input class="form-control" type="file" id="imagen" name="imagen" multiple>
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <div id="emailHelp" class="form-text">Descripción con una extension maxima a 30 caracteres.</div>
                 </div>
                 <div class="d-grid gap-2 col-6 mx-auto">
                     <button class="btn btn-secondary" type="submit">Button</button>

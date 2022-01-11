fb9f69a72940dfdf9935b5244df770a0adc8ebbd
Miguel-Simonet <84403278+Miguel-Simonet@users.noreply.github.com>
2022-01-11 17:10:26 +0100

    11/01/2022

--- Front-End/Componentes/Carrusel/Back-end-Carrusel/index.php
+++ Front-End/Componentes/Carrusel/Back-end-Carrusel/index.php
@@ -44,13 +44,33 @@
             </div>
         </div>
     </form>
-    <?php
+<?PHP
     function mysql_levelUp(){
         return new mysqli("localhost","root","","levelup");
     }
-    $mysql= mysql_levelUp();
-    $select=$mysql->query('select titulo from carrusel');
-    print_r($select);
-    ?>
+    $conn = mysql_levelUp();
+    $result=$conn->query("select * from carrusel");
+    $chequeo = mysqli_num_rows($result);
+    echo "<h1>Items</h1>";
+    echo "<div>";
+    if ($chequeo>0) {
+        echo "<table class='table'><tr><th scope='col'>Id</th><th scope='col'>titulo</th><th scope='col'>descripcion</th><th scope='col'>image</th><th scope='col'>created_at</th></tr>";
+        while ($row = mysqli_fetch_assoc($result)){ 
+            //for loop better than this line
+            echo "<tr>
+            <th scope='row'></th>
+                <td>" . $row['id'] . "</td>
+                <td>" . $row['titulo'] . "</td>
+                <td>" . $row['descripcion'] . "</td>
+                <td>" . $row['image'] . "</td>
+                <td>" . $row['created_at'] . "</td>
+            </tr>";
+        }
+        echo "</table></div>";
+    } else {
+        echo "No results<br>";
+    }
+    mysqli_close($conn);
+?>
 </body>
 </html>

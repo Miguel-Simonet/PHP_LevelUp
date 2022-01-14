e0ca9570c8361edeb0bf369b125a16489fce92a2
Miguel-Simonet <84403278+Miguel-Simonet@users.noreply.github.com>
2022-01-14 15:45:24 +0100

    14/01/2022

--- Front-End/Sesiones/Registro_Inicio_Sesion.php
+++ Front-End/Sesiones/Registro_Inicio_Sesion.php
@@ -9,12 +9,12 @@
   <!-- Fontawesome CDN Link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./CSS/sesion.css">
-  <!--<?php
+  <link rel="stylesheet" href="./CSS/sesion.css ">
+  <?php
       include("../../Boostrap_Thinks/boostrap.php");
-      ?>-->
+  ?>
 </head>
-<style>
+<style !important>
   /* Google Fonts Import Link */
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 
